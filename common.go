7b10553b3532991060778cdaa69fa1fac043836c
Joseph Borodach <95253429+JosephBorodach@users.noreply.github.com>
2026-06-05 10:00:28 -0400

    update registry namespace from viam-labs to viam (#8)
    Co-authored-by: JosephBorodach <joseph.borodach@viam.com>

--- common.go
+++ common.go
@@ -3,6 +3,6 @@ package toolchanger
 import "go.viam.com/rdk/resource"
 
 var (
-	NamespaceFamily = resource.ModelNamespace("viam-labs").WithFamily("tool-changer")
+	NamespaceFamily = resource.ModelNamespace("viam").WithFamily("tool-changer")
 	DefaultModel    = NamespaceFamily.WithModel("default")
 )
